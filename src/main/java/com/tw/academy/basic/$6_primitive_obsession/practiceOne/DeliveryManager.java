a39c1474fc4b8d76b95e8cbd2538b11d2f1391e7
GAOGA4 <gaovin.z.l.gao@oocl.com>
2021-06-17 21:18:04 +0800

    move getCity and getProvince to Address

--- src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceOne/DeliveryManager.java
+++ src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceOne/DeliveryManager.java
@@ -10,20 +10,14 @@ public class DeliveryManager {
     }
 
     public DeliverCenter allocate(){
-        if (getProvince(to.getAddress()).equals(getProvince(from.getAddress())) && getCity(to.getAddress()).equals(getCity(from.getAddress()))){
+        if (to.getProvince().equals(from.getProvince()) && to.getCity().equals(from.getCity())){
             return DeliverCenter.LOCAL;
         }
-        if (getProvince(to.getAddress()).equals(getProvince(from.getAddress()))) {
+        if (to.getProvince().equals(from.getProvince())) {
             return DeliverCenter.PROVINCE;
         }
         return DeliverCenter.FOREIGN;
     }
 
-    private String getCity(String address) {
-        return address.substring(address.indexOf("省") + 1, address.indexOf("市"));
-    }
 
-    private String getProvince(String address) {
-        return address.substring(0, address.indexOf("省"));
-    }
 }
