78f3b94719f334efea7c3f37c70a06711b610178
GAOGA4 <gaovin.z.l.gao@oocl.com>
2021-06-17 17:39:33 +0800

    init

---
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/Order.java
@@ -0,0 +1,24 @@
+package com.tw.academy.basic.$5_data_clumps;
+
+public class Order {
+
+    private int orderNumber;
+
+    private String buyerName;
+    private String buyerPhoneNumber;
+    private String buyerAddress;
+
+    public Order(int orderNumber, User user) {
+        this.orderNumber = orderNumber;
+
+        this.buyerName = user.getBuyerName();
+        this.buyerPhoneNumber = user.getBuyerPhoneNumber();
+        this.buyerAddress = user.getBuyerAddress();
+    }
+
+    public String confirm(){
+        return String.format("Please confirm buyer information: buyer name is %s, " +
+                "buyer phone number is %s and buyer address is %s.", buyerName, buyerPhoneNumber, buyerAddress);
+    }
+}
+
